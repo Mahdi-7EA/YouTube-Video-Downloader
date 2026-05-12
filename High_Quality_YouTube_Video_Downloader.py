cb90a17a58805c6f04dd46ef1d7d86311649e75e
mahbuburrahman21 <mahbuburahman2u5t@gmail.com>
2026-05-13 03:19:44 +0600

    Added gitignore

--- High_Quality_YouTube_Video_Downloader.py
+++
@@ -1,9 +0,0 @@
-import yt_dlp
-
-ydl_opts = {
-    'format': 'bestvideo[height<=1080][ext=mp4]+bestaudio[ext=m4a]/bestvideo[height<=1080]+bestaudio/best[height<=1080]',
-    'outtmpl': '%(title)s.%(ext)s',
-    'merge_output_format': 'mp4',
-}
-
-yt_dlp.YoutubeDL(ydl_opts).download(['YOUTUBE_VIDEO_URL'])
